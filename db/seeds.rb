66b6c90c5c9d3a8e3ee0b31213dd04aab741279e
Julio Quintana <julioeq29@gmail.com>
2019-02-21 12:48:42 +0000

    schema, validations, associations, attributes and seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying database..."
+
+Ingredient.destroy_all
+
+puts "Creating fake ingredients..."
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "pepermint")
+Ingredient.create(name: "olives")
+Ingredient.create(name: "basil")
+
+puts "Done, go drink and have fun"
